39f2b8be59c0fc354be5fdac4c1dfd73a85f716b
BaranovAlexandr <baranovale228@gmail.com>
2021-08-16 15:50:57 +0300

    ne ponal

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,109 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private SessionFactory sessionFactory = Util.getSessionFactory();
+    private Session session = sessionFactory.openSession();;
+    private Transaction transaction = null;
     public UserDaoHibernateImpl() {
-
     }
 
 
     @Override
     public void createUsersTable() {
+        String sql = "CREATE TABLE  if not exists U1 (id BIGINT not null auto_increment," +
+                " name VARCHAR(30), lastname VARCHAR(30), age TINYINT UNSIGNED, primary key (id))";
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "DROP table if exists u1";
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            transaction = session.beginTransaction();
+            session.save(new User(name,lastName,age));
+            System.out.printf("User с именем – %s добавлен в базу данных\n", name);
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        try {
+            transaction = session.beginTransaction();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<User> cq = cb.createQuery(User.class);
+            Root<User> rootEntry = cq.from(User.class);
+            CriteriaQuery<User> all = cq.select(rootEntry);
+
+            TypedQuery<User> allQuery = session.createQuery(all);
+
+            transaction.commit();
+            return allQuery.getResultList();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 }
